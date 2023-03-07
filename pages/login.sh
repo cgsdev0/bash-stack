d9288cd2d0f66ff3978af22396f46ebcd1f2e902
Sarah Schulte <git@sarah.engineer>
2023-03-07 13:52:43 -0800

    i was missing a newline

--- pages/login.sh
+++ pages/login.sh
@@ -4,3 +4,4 @@ PLAYER_ID=$(echo "${FORM_DATA["username"]}" | sed 's/[^a-zA-Z_0-9]//g' | head -c
 printf "%s\r\n" "Set-Cookie: username=${PLAYER_ID}; Path=/"
 printf "%s\r\n" "HX-Refresh: true"
 printf "\r\n"
+printf "\r\n"
