186e50f017c8cde0eafe7e3f2ea4d04822aed757
Sarah Schulte <git@sarah.engineer>
2023-03-09 18:18:18 -0800

    streamer overlay view

---
+++ pages/stream/[room].sh
@@ -0,0 +1,30 @@
+
+ROOM_CODE="${REQUEST_PATH##*/}"
+
+htmx_page << EOF
+<div hx-sse="connect:/sse/host/${ROOM_CODE}">
+  <style>
+    .score-controls {
+      display: none;
+    }
+    body {
+      background-image: initial;
+    }
+    #signature {
+      display: none;
+    }
+    .player {
+      min-height: initial;
+      flex-grow: 1;
+    }
+    .players {
+      margin-top: initial;
+    }
+    .player-wrapper {
+      display: flex;
+      flex-grow: 1;
+    }
+  </style>
+    <div hx-trigger="load, sse:score, sse:leave, sse:join, sse:buzz, sse:unlock" hx-get="/room/players/${ROOM_CODE}" class="players"></div>
+</div>
+EOF
