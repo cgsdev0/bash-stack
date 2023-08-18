5a16fb74d7c9d565bd3747250ea8ff66cd8e4430
Sarah Schulte <git@sarah.engineer>
2023-08-18 06:29:45 -0700

    we do a lil caching nbd

--- examples/docs/pages/[[...route]].sh
+++ examples/docs/pages/[[...route]].sh
@@ -1,4 +1,6 @@
 
+mkdir -p cache
+
 INDEX="0-index"
 STRIPPED=${REQUEST_PATH#\/}
 STRIPPED=${STRIPPED:-$INDEX}
@@ -12,18 +14,29 @@ TOC=""
 if [[ "$STRIPPED" != "$INDEX" ]]; then
   TOC="--toc"
 else
-  GLOBAL_TOC="<ul>"
-  for FILE in data/*.md; do
-    FNAME="$(basename $FILE)"
-    FNAME="${FNAME%.md}"
-    GENERATED="$(pandoc --from markdown --to html --standalone --wrap=preserve --highlight-style=breezeDark $FILE --toc)"
-    GLOBAL_TOC+="<li>"
-    GLOBAL_TOC+="<a href='#'>$(echo "$GENERATED" | xmllint --html --xpath '//*[@id="title-block-header"]/h1/text()' - 2> /dev/null)</a>"
-    GLOBAL_TOC+="$(echo "$GENERATED" | xmllint --html --xpath '//*[@id="TOC"]/ul' - 2> /dev/null | sed 's@href="@href="/'$FNAME'@g')"
-    GLOBAL_TOC+="</li>"
-  done
-  GLOBAL_TOC+="</ul>"
-  GLOBAL_TOC="$(echo "$GLOBAL_TOC" | tr -d '\n')"
+  if [[ ! -f cache/global_toc ]] || [[ ! -z "$NO_CACHE" ]]; then
+    GLOBAL_TOC="<ul>"
+    for FILE in data/*.md; do
+      FNAME="$(basename $FILE)"
+      FNAME="${FNAME%.md}"
+      GENERATED="$(pandoc --from markdown --to html --standalone --wrap=preserve --highlight-style=breezeDark $FILE --toc)"
+      GLOBAL_TOC+="<li>"
+      GLOBAL_TOC+="<a href='#'>$(echo "$GENERATED" | xmllint --html --xpath '//*[@id="title-block-header"]/h1/text()' - 2> /dev/null)</a>"
+      GLOBAL_TOC+="$(echo "$GENERATED" | xmllint --html --xpath '//*[@id="TOC"]/ul' - 2> /dev/null | sed 's@href="@href="/'$FNAME'@g')"
+      GLOBAL_TOC+="</li>"
+    done
+    GLOBAL_TOC+="</ul>"
+    GLOBAL_TOC="$(echo "$GLOBAL_TOC" | tr -d '\n')"
+
+    # save to cache
+    if [[ -z "$NO_CACHE" ]]; then
+      debug "cache miss :("
+      echo "$GLOBAL_TOC" > cache/global_toc
+    fi
+  else
+    debug "cache hit"
+    GLOBAL_TOC="$(cat cache/global_toc)"
+  fi
 fi
 htmx_page << EOF
   $(pandoc --from markdown --to html --standalone --wrap=preserve --highlight-style=breezeDark $MARKDOWN_FILE $TOC \
