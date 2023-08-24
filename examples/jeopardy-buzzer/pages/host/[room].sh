8e59aa66eed276ece4f8c8563071865bdb4334c6
Sarah Schulte <git@sarah.engineer>
2023-08-24 14:04:01 -0700

    jeopardy buzzer fix

--- examples/jeopardy-buzzer/pages/host/[room].sh
+++ examples/jeopardy-buzzer/pages/host/[room].sh
@@ -2,33 +2,32 @@
 ROOM_CODE="${REQUEST_PATH##*/}"
 
 htmx_page << EOF
+  <div hx-sse="connect:/sse/host/${ROOM_CODE}">
+      <div hx-trigger="sse:kick, sse:leave, sse:join, sse:unlock, sse:buzz, load" hx-get="/room/players/${ROOM_CODE}" class="players"></div>
+      <form>
+      <input type="hidden" name="room_code" value="${ROOM_CODE}" />
+      <div class="button-container">
+        <button hx-post="/reset" hx-sse="swap:buzz">Reset</button>
+      </div>
+      </form>
+  </div>
+  <form class="footer-link copy">
+    <input type="text" value="http://${HTTP_HEADERS["host"]}/player/${ROOM_CODE}">
+    <button type="button">Copy</button>
+  </form>
+  <script type="text/javascript">
+  (function() {
+    var copyButton = document.querySelector('.copy button');
+    var copyInput = document.querySelector('.copy input');
+    copyButton.addEventListener('click', function(e) {
+      e.preventDefault();
+      var text = copyInput.select();
+      document.execCommand('copy');
+    });
 
-<div hx-sse="connect:/sse/host/${ROOM_CODE}">
-    <div hx-trigger="sse:kick, sse:leave, sse:join, sse:unlock, sse:buzz, load" hx-get="/room/players/${ROOM_CODE}" class="players"></div>
-    <form>
-    <input type="hidden" name="room_code" value="${ROOM_CODE}" />
-    <div class="button-container">
-      <button hx-post="/reset" hx-sse="swap:buzz">Reset</button>
-    </div>
-    </form>
-</div>
-<form class="footer-link copy">
-  <input type="text" value="http://${HTTP_HEADERS["Host"]}/player/${ROOM_CODE}">
-  <button type="button">Copy</button>
-</form>
-<script type="text/javascript">
-(function() {
-  var copyButton = document.querySelector('.copy button');
-  var copyInput = document.querySelector('.copy input');
-  copyButton.addEventListener('click', function(e) {
-    e.preventDefault();
-    var text = copyInput.select();
-    document.execCommand('copy');
-  });
-
-  copyInput.addEventListener('click', function() {
-    this.select();
-  });
-})();
-</script>
+    copyInput.addEventListener('click', function() {
+      this.select();
+    });
+  })();
+  </script>
 EOF
